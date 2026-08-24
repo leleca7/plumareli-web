3bfa0328501c47238f7d8aa8b411508c16a61d7f
leleca7 <wanelle52@gmail.com>
2026-08-23 23:36:16 -0300

    chore: keep image studio hosted by Supabase

--- studio/lib/config.ts
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = "https://ghpqnqxjxmdmhikdacoq.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6HBOy2srshYvkQ-7ufzzew_LKdspq7M";
-export const STUDIO_BUCKET = "plumareli-image-studio";
